4a0eb7e0dfaa63e99a09a4a7c40fe0da6b16a8cb
Richard <rdesilvey@gmail.com>
2017-04-03 12:18:21 -0600

    Final addons

--- app/src/main/java/com/example/richard/flashbeepshake/MainActivity.java
+++ app/src/main/java/com/example/richard/flashbeepshake/MainActivity.java
@@ -2,16 +2,11 @@ package com.example.richard.flashbeepshake;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +14,6 @@ public class MainActivity extends AppCompatActivity implements Shaker.Callback {
     private Shaker shaker=null;
     private Vibrator vibrator;
     private ToneGenerator toneGen;
-    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +24,6 @@ public class MainActivity extends AppCompatActivity implements Shaker.Callback {
         vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
         toneGen = new ToneGenerator(AudioManager.STREAM_MUSIC, 100000);
 
-        Boolean isFlashAvailable = getApplicationContext()
-                .getPackageManager()
-                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-        if (isFlashAvailable){
-            Toast.makeText(this, "Flash is supported on this device", Toast.LENGTH_LONG);
-        }
     }
 
     @Override
@@ -57,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements Shaker.Callback {
         view.setText("Calm");
         vibrator.cancel();
         toneGen.stopTone();
+
     }
 
     private boolean isFlashSupported() {
