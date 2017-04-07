5636f6e9acd2f4369d6b23b61d05d559fea1b0b3
Richard <rdesilvey@gmail.com>
2017-04-07 12:56:13 -0600

    Finished the app

--- app/src/main/java/com/example/richard/flashbeepshake/MainActivity.java
+++ app/src/main/java/com/example/richard/flashbeepshake/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.richard.flashbeepshake;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity implements Shaker.Callback {
     private Shaker shaker=null;
     private Vibrator vibrator;
     private ToneGenerator toneGen;
+    private Camera cam;
+    private boolean on = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,29 +31,55 @@ public class MainActivity extends AppCompatActivity implements Shaker.Callback {
     }
 
     @Override
+    protected void onResume(){
+        super.onResume();
+
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         shaker.close();
+        cam.release();
+    }
+
+    public void toggleFlashlight(View v) {
+        if (!on) {
+            cam = Camera.open();
+            Camera.Parameters p = cam.getParameters();
+            p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            cam.setParameters(p);
+            cam.startPreview();
+            on = true;
+        }else{
+            cam.stopPreview();
+            cam.release();
+            on = false;
+        }
+    }
+
+    public void startVibrate(int mils){
+        vibrator.vibrate(mils);
+    }
+
+    public void haltVibrate(){
+        vibrator.cancel();
+    }
+
+    public void startTone(View v){
+        toneGen.startTone(ToneGenerator.TONE_CDMA_PIP,1000);
     }
 
     public void shakingStarted() {
         TextView view = (TextView) findViewById(R.id.output);
         view.setText("SHAKING!!!");
-        vibrator.vibrate(10000);
-        toneGen.startTone(ToneGenerator.TONE_CDMA_PIP,100);
-
+        startVibrate(10000);
     }
 
     public void shakingStopped() {
         TextView view = (TextView) findViewById(R.id.output);
         view.setText("Calm");
-        vibrator.cancel();
-        toneGen.stopTone();
-
+        haltVibrate();
     }
 
-    private boolean isFlashSupported() {
-        PackageManager pm = getPackageManager();
-        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-    }
 }
